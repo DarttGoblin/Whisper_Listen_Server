dd73965c7dda73e43018242476d5e8833310b5de
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2024-08-03 20:24:19 +0100

    Update Listen.js

--- Listen.js
+++ Listen.js
@@ -1,3 +1,5 @@
+console.log("i am working");
+
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
@@ -5,10 +7,20 @@ const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, {cors: {origin: "*", methods: ["GET", "POST"]}});
+const io = socketIo(server, {
+    cors: {
+        origin: "https://darttgoblin.github.io",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({
+    origin: "https://darttgoblin.github.io",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"]
+}));
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -16,7 +28,10 @@ app.use((req, res, next) => {
     next();
 });
 
+console.log("i am still working");
+
 io.on('connection', socket => {
+    console.log("sockets are working");
     socket.on('user-connected', user => {
         socket.user = user; 
         socket.emit('recieve-user-connected', user)
